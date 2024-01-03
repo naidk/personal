a686d19dc5ffa32d231de4dc3f7ec2a7b1bf3576
naidk <80177026+naidk@users.noreply.github.com>
2024-01-04 03:00:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -222,137 +222,369 @@
 //   console.error('Error starting server:', err);
 // });
 
+// const express = require('express');
+// const { ApolloServer } = require('apollo-server-express');
+// const { connect, Schema, model } = require('mongoose');
+
+// const MONGODB = 'mongodb+srv://root:root@cluster0.axpr6ia.mongodb.net/myapplication';
+
+// const bookSchema = new Schema({
+//     id: String,
+//     author: { type: String, required: true },
+//     title: { type: String, required: true },
+//     year: { type: Date, required: true }
+// });
+
+// const Book = model('books', bookSchema);
+
+// const typeDefs = `
+//     type Book {
+//         _id: String
+//         author: String
+//         title: String
+//         year: String
+//     }
+
+//     input BookFilter {
+//         ids: [ID!]
+//         author: String
+//         startDate: String
+//         endDate: String
+//     }
+
+//     input BookFiltersInput {
+//         filter: BookFilter
+//         limit: Int
+//     }
+
+//     input BookInput {
+//         author: String
+//         title: String
+//         year: String
+//     }
+
+//     type Query {
+//         getBook(ID: ID!): Book
+//         getBooks(limit: Int): [Book]!
+//         books(input: BookFiltersInput): [Book]!
+//     }
+
+//     type Mutation {
+//         createBook(bookInput: BookInput): String!
+//         updateBook(ID: ID!, bookInput: BookInput): String!
+//         deleteBook(ID: ID!): String!
+//     }
+// `;
+
+// const resolvers = {
+//     Query: {
+//         async getBook(_, { ID }) {
+//             return await Book.findById(ID);
+//         },
+//         async getBooks(_, { limit }) {
+//             return await Book.find().limit(limit);
+//         },
+//         async books(_, args) {
+//             const { filter } = args.input;
+//             const { limit } = args;
+
+//             const shouldApplyFilter = filter != null;
+//             if (!shouldApplyFilter) {
+//                 return await Book.find().limit(limit);
+//             }
+
+//             const shouldApplyIDsFilter = filter.ids != null;
+//             if (shouldApplyIDsFilter) {
+//                 let ids = filter.ids;
+//                 ids = ids.filter(id => Types.ObjectId.isValid(id));
+//                 return await Book.find({ "_id": { "$in": ids } });
+//             }
+
+//             const shouldApplyAuthorFilter = filter.author != null;
+//             if (shouldApplyAuthorFilter) {
+//                 return await Book.find({ "author": filter.author });
+//             }
+
+//             const shouldApplyDateRangeFilter = filter.startDate != null && filter.endDate != null;
+//             if (shouldApplyDateRangeFilter) {
+//                 let startDate = filter.startDate;
+//                 let endDate = filter.endDate;
+//                 return await Book.find({ "year": { "$gte": startDate, "$lte": endDate } });
+//             }
+//         }
+//     },
+//     Mutation: {
+//         async createBook(_, { bookInput: { author, title, year } }) {
+//             const res = await new Book({ author, title, year }).save();
+//             return res._id;
+//         },
+//         async updateBook(_, { ID, bookInput: { author, title, year } }) {
+//             await Book.updateOne({ _id: ID }, { $set: { author, title, year } });
+//             return ID;
+//         },
+//         async deleteBook(_, { ID }) {
+//             await Book.remove({ _id: ID });
+//             return ID;
+//         }
+//     }
+// };
+
+// async function startServer() {
+//     try {
+//         await connect(MONGODB, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+
+//         console.log('Connected to MongoDB');
+
+//         const server = new ApolloServer({ typeDefs, resolvers });
+//         const app = express();
+
+//         server.applyMiddleware({ app });
+
+//         const port = 4000;
+
+//         app.listen({ port }, () =>
+//             console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`)
+//         );
+//     } catch (error) {
+//         console.error('Error connecting to MongoDB:', error);
+//     }
+// }
+
+// startServer().catch((err) => {
+//     console.error('Error starting server:', err);
+// });
+
+
+const { ApolloServer } = require('@apollo/server');
+const { gql } = require('graphql-tag');
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { connect, Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
 
-const MONGODB = 'mongodb+srv://root:root@cluster0.axpr6ia.mongodb.net/myapplication';
+// Load environment variables
+require('dotenv').config();
 
-const bookSchema = new Schema({
-    id: String,
-    author: { type: String, required: true },
-    title: { type: String, required: true },
-    year: { type: Date, required: true }
+const MONGODB = process.env.MONGODB_URI;
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const GMAIL_USER = process.env.GMAIL_USER;
+const GMAIL_PASS = process.env.GMAIL_PASS;
+
+const User = mongoose.model('User', {
+  phoneNumber: String,
+  verificationCode: String,
+  verificationCodeTimestamp: Date,
+  email: String,
+  emailVerificationCode: String,
+  emailVerificationCodeExpiration: Date,
+  firstName: String,
+  lastName: String,
+  dob: Date,
+  gender: String,
+  weight: Number,
+  height: Number
 });
 
-const Book = model('books', bookSchema);
+const accountSid = TWILIO_ACCOUNT_SID;
+const authToken = TWILIO_AUTH_TOKEN;
+const twilioClient = require('twilio')(accountSid, authToken);
 
-const typeDefs = `
-    type Book {
-        _id: String
-        author: String
-        title: String
-        year: String
-    }
+const JWT_SECRET = process.env.JWT_SECRET;
 
-    input BookFilter {
-        ids: [ID!]
-        author: String
-        startDate: String
-        endDate: String
-    }
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: GMAIL_USER,
+    pass: GMAIL_PASS
+  }
+});
 
-    input BookFiltersInput {
-        filter: BookFilter
-        limit: Int
-    }
+const typeDefs = gql`
+  type Query {
+    hello: String
+    getUser(phoneNumber: String!): User
+  }
 
-    input BookInput {
-        author: String
-        title: String
-        year: String
-    }
+  type Mutation {
+    initiatePhoneAuthentication(phoneNumber: String!): String
+    initiateEmailAuthentication(email: String!): String
+    verifyPhoneAuthentication(phoneNumber: String!, verificationCode: String!): String
+    verifyEmailAuthentication(email: String!, verificationCode: String!): String
+    createUser(input: UserInput): User
+    updateUser(phoneNumber: String!, input: UserInput): User
+    deleteUser(phoneNumber: String!): User
+  }
 
-    type Query {
-        getBook(ID: ID!): Book
-        getBooks(limit: Int): [Book]!
-        books(input: BookFiltersInput): [Book]!
-    }
+  input UserInput {
+    phoneNumber: String!
+    firstName: String
+    lastName: String
+    dob: String
+    gender: String
+    weight: Float
+    height: Float
+  }
 
-    type Mutation {
-        createBook(bookInput: BookInput): String!
-        updateBook(ID: ID!, bookInput: BookInput): String!
-        deleteBook(ID: ID!): String!
-    }
+  type User {
+    phoneNumber: String
+    verificationCode: String
+    firstName: String
+    lastName: String
+    dob: String
+    gender: String
+    weight: Float
+    height: Float
+  }
 `;
 
 const resolvers = {
-    Query: {
-        async getBook(_, { ID }) {
-            return await Book.findById(ID);
-        },
-        async getBooks(_, { limit }) {
-            return await Book.find().limit(limit);
-        },
-        async books(_, args) {
-            const { filter } = args.input;
-            const { limit } = args;
+  Query: {
+    hello: () => 'Hello, GraphQL!',
+    getUser: async (_, { phoneNumber }) => User.findOne({ phoneNumber })
+  },
+  Mutation: {
+    initiatePhoneAuthentication: async (_, { phoneNumber }) => {
+      const user = await User.findOne({ phoneNumber });
+      const verificationCode = Math.floor(1000 + Math.random() * 9000).toString();
 
-            const shouldApplyFilter = filter != null;
-            if (!shouldApplyFilter) {
-                return await Book.find().limit(limit);
-            }
+      if (!user) {
+        await User.create({ phoneNumber, verificationCode, verificationCodeTimestamp: new Date() });
+      } else {
+        await User.findOneAndUpdate({ phoneNumber }, { verificationCode, verificationCodeTimestamp: new Date() });
+      }
 
-            const shouldApplyIDsFilter = filter.ids != null;
-            if (shouldApplyIDsFilter) {
-                let ids = filter.ids;
-                ids = ids.filter(id => Types.ObjectId.isValid(id));
-                return await Book.find({ "_id": { "$in": ids } });
-            }
+      try {
+        await twilioClient.messages.create({
+          body: `Your verification code is: ${verificationCode}`,
+          to: phoneNumber,
+          from: '+14403055697'
+        });
+        console.log(`Verification code sent to ${phoneNumber}`);
+      } catch (error) {
+        console.error('Error sending SMS:', error);
+        throw new Error('Failed to send verification code: ' + error.message);
+      }
 
-            const shouldApplyAuthorFilter = filter.author != null;
-            if (shouldApplyAuthorFilter) {
-                return await Book.find({ "author": filter.author });
-            }
-
-            const shouldApplyDateRangeFilter = filter.startDate != null && filter.endDate != null;
-            if (shouldApplyDateRangeFilter) {
-                let startDate = filter.startDate;
-                let endDate = filter.endDate;
-                return await Book.find({ "year": { "$gte": startDate, "$lte": endDate } });
-            }
-        }
+      return jwt.sign({ phoneNumber, verificationCode }, JWT_SECRET);
     },
-    Mutation: {
-        async createBook(_, { bookInput: { author, title, year } }) {
-            const res = await new Book({ author, title, year }).save();
-            return res._id;
-        },
-        async updateBook(_, { ID, bookInput: { author, title, year } }) {
-            await Book.updateOne({ _id: ID }, { $set: { author, title, year } });
-            return ID;
-        },
-        async deleteBook(_, { ID }) {
-            await Book.remove({ _id: ID });
-            return ID;
+    verifyPhoneAuthentication: async (_, { phoneNumber, verificationCode }) => {
+      const user = await User.findOne({ phoneNumber });
+
+      if (!user || user.verificationCode !== verificationCode) {
+        throw new Error('Invalid verification code');
+      }
+
+      const timeLimitInMinutes = 5;
+      const currentTime = new Date();
+      const timeDiff = Math.abs(currentTime - user.verificationCodeTimestamp) / (1000 * 60);
+
+      if (timeDiff > timeLimitInMinutes) {
+        throw new Error('Verification code has expired');
+      }
+
+      return jwt.sign({ phoneNumber, verificationCode }, JWT_SECRET);
+    },
+   
+    initiateEmailAuthentication: async (_, { email }) => {
+      try {
+        let user = await User.findOne({ email });
+
+        if (!user) {
+          user = await User.create({ email });
         }
+
+        const currentTime = new Date();
+        const timeLimitInMinutes = 5;
+        const expirationTime = new Date(currentTime.getTime() + timeLimitInMinutes * 60000);
+
+        const verificationCode = Math.floor(1000 + Math.random() * 9000).toString();
+        user.emailVerificationCode = verificationCode;
+        user.emailVerificationCodeExpiration = expirationTime;
+        await user.save();
+
+        const mailOptions = {
+          from: GMAIL_USER,
+          to: email,
+          subject: 'Email Verification Code',
+          text: `Your email verification code is: ${verificationCode}`,
+        };
+
+        await transporter.sendMail(mailOptions);
+        console.log(`Email verification code sent to ${email}`);
+        console.log(`Code expires at: ${expirationTime}`);
+
+        return 'Email verification code sent successfully';
+      } catch (error) {
+        console.error('Error initiating email authentication:', error);
+        throw new Error('Failed to initiate email authentication');
+      }
+    },
+
+    verifyEmailAuthentication: async (_, { email, verificationCode }) => {
+      try {
+        const user = await User.findOne({ email });
+
+        if (!user || user.emailVerificationCode !== verificationCode) {
+          throw new Error('Invalid verification code');
+        }
+
+        const currentTime = new Date();
+        if (user.emailVerificationCodeExpiration && currentTime > user.emailVerificationCodeExpiration) {
+          throw new Error('Email verification code has expired');
+        }
+
+        user.emailVerificationCode = null;
+        user.emailVerificationCodeExpiration = null;
+        await user.save();
+
+        return 'Email verification successful';
+      } catch (error) {
+        console.error('Error verifying email authentication:', error);
+        throw new Error('Failed to verify email authentication');
+      }
+    },
+    createUser: async (_, { input }) => {
+      const user = await User.create(input);
+      return user;
+    },
+    updateUser: async (_, { phoneNumber, input }) => {
+      const user = await User.findOneAndUpdate({ phoneNumber }, input, { new: true });
+      return user;
+    },
+    deleteUser: async (_, { phoneNumber }) => {
+      const user = await User.findOneAndDelete({ phoneNumber });
+      return user;
     }
+  }
 };
 
 async function startServer() {
-    try {
-        await connect(MONGODB, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+  try {
+    await mongoose.connect(MONGODB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB');
 
-        const server = new ApolloServer({ typeDefs, resolvers });
-        const app = express();
+    const server = new ApolloServer({ typeDefs, resolvers });
+    const app = express();
 
-        server.applyMiddleware({ app });
+    const { url } = await startStandaloneServer(server);
 
-        const port = 4000;
-
-        app.listen({ port }, () =>
-            console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`)
-        );
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-    }
+    console.log(`Server ready at ${url}`);
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
 }
 
 startServer().catch((err) => {
-    console.error('Error starting server:', err);
+  console.error('Error starting server:', err);
 });
